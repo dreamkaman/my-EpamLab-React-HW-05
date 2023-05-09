3ae6803693d0a976b055b11437a8d3c79d096695
Dreamkaman <riabushko@gmail.com>
2023-05-09 23:10:17 +0300

    Next

--- src/components/CourseForm/tests/CourseForm.test.js
+++ src/components/CourseForm/tests/CourseForm.test.js
@@ -1,11 +1,33 @@
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import * as reactReduxHooks from 'redux/hooks.ts';
 import CourseForm from '../CourseForm';
 import { renderWithProvider } from 'utils/test-utils';
 
 describe('Tests for CourseForm', () => {
-	renderWithProvider(<CourseForm mode={'create'} />);
+	renderWithProvider(<CourseForm mode='create' />);
+
+	screen.debug();
 
 	it('CourseForm should show authors lists (all and course authors).', () => {
 		expect(screen.getAllByText(/TestAuthor/).length).toBe(2);
+	});
+
+	it('CourseForm "Create author" click button should call dispatch.', () => {
+		jest.mock('redux/hooks.ts');
+
+		const mockedDispatch = jest.spyOn(reactReduxHooks, 'useAppDispatch');
+
+		const dispatch = jest.fn();
+		mockedDispatch.mockReturnValue(dispatch);
+
+		renderWithProvider(<CourseForm mode={'create'} />);
+
+		const btnCreateAuthor = screen.getByText('Create author');
+
+		console.log(btnCreateAuthor);
+
+		fireEvent.click(btnCreateAuthor);
+
+		// expect(dispatch).toHaveBeenCalled();
 	});
 });
